33853706a726d0ffeaba1be67cdc57a48a57575d
christian <christian.mader@iais.fraunhofer.de>
2019-03-14 15:25:30 +0100

    implement all tests

--- src/test/java/DeserializeInstantiatedClass.java
+++ src/test/java/DeserializeInstantiatedClass.java
@@ -1,14 +1,27 @@
+import de.fraunhofer.iais.eis.BaseConnector;
+import de.fraunhofer.iais.eis.InfrastructureComponent;
 import de.fraunhofer.iais.eis.ids.jsonld.Serializer;
+import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.StringWriter;
 
 public class DeserializeInstantiatedClass {
 
     private Serializer serializer = new Serializer();
 
     @Test
-    public void deserialize() {
-        Assert.fail();
+    public void deserialize() throws IOException {
+        StringWriter writer = new StringWriter();
+        IOUtils.copy(this.getClass().getResourceAsStream("BaseConnector.jsonld"), writer);
+        String serializedConnectorDescription = writer.toString();
+
+        InfrastructureComponent deserialized = serializer.deserialize(serializedConnectorDescription, InfrastructureComponent.class);
+
+        Assert.assertNotNull(deserialized);
+        Assert.assertTrue(deserialized instanceof BaseConnector);
     }
 
 }
--- src/test/java/SerializeInstantiatedClass.java
+++ src/test/java/SerializeInstantiatedClass.java
@@ -1,24 +1,54 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import de.fraunhofer.iais.eis.*;
 import de.fraunhofer.iais.eis.ids.jsonld.Serializer;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static de.fraunhofer.iais.eis.util.Util.asList;
 
 public class SerializeInstantiatedClass {
 
     private Serializer serializer = new Serializer();
+    private BaseConnector baseConnector;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        baseConnector = new BaseConnectorBuilder()
+            ._maintainer_(new URL("http://www.iais.fraunhofer.de/"))
+            ._curator_(new URL("http://www.iais.fraunhofer.de/"))
+            ._catalog_(new CatalogBuilder().build())
+            ._outboundModelVersion_("1.0.2")
+            ._inboundModelVersions_(asList("1.0.2"))
+            .build();
+    }
 
     @Test
     public void serializeToJsonLD_fromObject() {
-        Assert.fail();
+        String serializedJsonLD = baseConnector.toRdf();
+
+        Assert.assertTrue(serializedJsonLD.contains("@id"));
+        Assert.assertTrue(serializedJsonLD.contains("@context"));
     }
 
     @Test
-    public void serializeToJsonLD_bySerializerCall() {
-        Assert.fail();
+    public void serializeToJsonLD_bySerializerCall() throws IOException {
+        String serializedJsonLD = serializer.serialize(baseConnector);
+
+        Assert.assertTrue(serializedJsonLD.contains("@id"));
+        Assert.assertTrue(serializedJsonLD.contains("@context"));
     }
 
     @Test
-    public void serializeToJson() {
-        Assert.fail();
+    public void serializeToJson() throws JsonProcessingException {
+        String serializedJson = serializer.serializePlainJson(baseConnector);
+
+        Assert.assertFalse(serializedJson.contains("@context"));
+        Assert.assertTrue(serializedJson.contains("@id"));
     }
 
 }
